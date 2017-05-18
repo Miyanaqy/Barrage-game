ad194c16bd488175523ff66f35ef629c322977e7
Miyanaqy <2247762766@qq.com>
2017-05-18 17:38:02 +0800

    upload

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import pygame, sys, math, random
 
 class OwnFly(pygame.sprite.Sprite):
     def __init__(self,image_file,location):
-        self.image_file = image_file
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.center = location
+        self.imagesum = 0
+    def images(self):
+        self.imagesum += 1
+        if self.imagesum >12 :
+            self.imagesum = 0
+        self.image = pygame.image.load('image/own%s.png' % ((self.imagesum/4)+1))
+        return self.image
 
 class BulletClass(pygame.sprite.Sprite):
     def __init__(self,image_file,pos,epos, speed = [0,0]):
@@ -22,6 +28,8 @@ class BulletClass(pygame.sprite.Sprite):
         self.speed = [(epos[0]-pos[0]) / s, (epos[1]-pos[1]) / s]
     def move(self):
         self.rect = self.rect.move(self.speed)
+
+
 
 
 def mox(x):
@@ -48,36 +56,39 @@ def fonts(text, size, x, y):
 pygame.init()
 screen = pygame.display.set_mode([450,700])
 screen.fill([255,255,255])
-own = OwnFly('image/own.png',[250,350])
+own = OwnFly('image/own1.png',[250,350])
 pygame.key.set_repeat(12,12)
 bullets = pygame.sprite.Group()
 clock = pygame.time.Clock()
 global score
 score = 0
+maxs = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                own.rect.centery -= 4
-            if event.key == pygame.K_DOWN:
-                own.rect.centery += 4
-            if event.key == pygame.K_LEFT:
-                own.rect.centerx -= 4
-            if event.key == pygame.K_RIGHT:
-                own.rect.centerx += 4
+            if event.key == pygame.K_UP and own.rect.centery-10>0:
+                own.rect.centery -= 6
+            if event.key == pygame.K_DOWN and own.rect.centery+10<700 :
+                own.rect.centery += 6
+            if event.key == pygame.K_LEFT and own.rect.centerx-10>0:
+                own.rect.centerx -= 6
+            if event.key == pygame.K_RIGHT and own.rect.centerx+10<450:
+                own.rect.centerx += 6
     clock.tick(50)
     screen.fill([255,255,255])
     b = random.randint(0,2)
     if b  == 1:
-        intx = random.randint(0,450)
-        inty = random.randint(0,700)
-        bullet = BulletClass('image/Bullet1.png',[intx,inty], own.rect)
+        angle = random.randint(0,360)
+        intx = 100 * math.sin(angle*math.pi/180)+120
+        inty = 100 * math.cos(angle*math.pi/180)+120
+        maxs += 1
+        bullet = BulletClass('image/Bullet1.png',[intx,inty], own.rect.center)
         bullets.add(bullet)
 
-    screen.blit(own.image, own.rect)
+    screen.blit(own.images(), own.rect)
     anim(bullets, own)
-    fonts('on: %s' % score,16,30,30)
+    fonts('on: %s/%s' % (score,maxs),16,30,30)
     pygame.display.flip()
 
